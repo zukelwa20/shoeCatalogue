d1c5692b3d6bd625fbbaf6402d58783499fde096
bootcamp <makhosandilezukelwa@gmail.com>
2017-04-20 17:37:55 +0200

    worked on js

--- shoeCatalogue.js
+++ shoeCatalogue.js
@@ -26,51 +26,51 @@ var shoeDataList = [
   {Brand: "LuckSneakers", Color: "Red", Size:8, Price:"R550"},
   {Brand: "LuckSneakers", Color: "Red", Size:9, Price:"R550"},
   {Brand: "LuckSneakers", Color: "Red", Size:10, Price:"R550"}
-
 ]
-// var myOptionsTemplate = document.querySelector(".myOptionsTemplate");
-//var templateOptions = Handlebars.compile(myOptionsTemplate.innerHTML);
-
-
 
  var myTemplate = document.querySelector(".myTemplate");
  var template = Handlebars.compile(myTemplate.innerHTML);
 
-// (function(){
-//
-//   var shoeStock = template({ shoesList: shoeDataList })
-//   stock.innerHTML = shoeStock;
-//
-// })
 
 
-
- var shoeFilter=[];
+  var shoeFilter=[];
   searchBut.addEventListener("click", function(){
+
+
   var brandClass= document.querySelector(".brandClass");
   var colorClass=document.querySelector(".colorClass");
   var sizeClass= document.querySelector(".sizeClass");
   var selectedBrand = brandClass.value;
   var selectedColor = colorClass.value;
   var selectedSize = Number(sizeClass.value);
-// shoeData = shoeDataList[i]
-//    if(selectedBrand =="" && selectedColor =="" && selectedSize ==""){
-//        stock.push(shoeDataList);
-//   stock.innerHTML  = template({ shoesList: shoeDataList })
-//    }
- for (var i = 0; i < shoeDataList.length; i++) {
-  if(selectedBrand === shoeDataList[i].Brand
-  && selectedColor === shoeDataList[i].Color
-  && selectedSize === shoeDataList[i].Size){
-    shoeFilter.push(shoeDataList);
-  }
+
+for (var i = 0; i < shoeDataList.length; i++) {
+var shoeData = shoeDataList[i];
+var brandMatch = false;
+if(selectedBrand === shoeData.Brand){
+    brandMatch = shoeData;
 }
-  stock.innerHTML = template({ shoesList: shoeFilter,
-                              shoesList: shoeDataList})
+if(brandMatch){
+shoeFilter.push(shoeData)
+}
+}
 
 
 
-});
+//
+//  for (var i = 0; i < shoeDataList.length; i++) {
+//    shoeData = shoeDataList[i];
+//   if(selectedBrand === shoeData.Brand
+//   && selectedColor === shoeData.Color
+//   && selectedSize === shoeData.Size){
+//     shoeFilter.push(shoeData);
+//   }
+//   }
+//
+// stock.innerHTML = template({shoesList: shoeDataList})
+//
+//
+ });
 
 var tempDisplay = document.querySelector(".tempDisplay");
 var filterTemplate = document.querySelector(".filterTemplate");
@@ -109,33 +109,7 @@ tempDisplay.innerHTML = filterTempOptions({ Brand: shoeDataList,
                                             Color: shoeDataList,
                                             Size: shoeDataList,
                                             Price: shoeDataList});
-                                          });
-
-// if(brandB !==""){
-//   shoeDataList.push()
-
-
-
-
-// else {
-//        if(checkedColor.filter(clickedOption)){
-//          shoeFilter.push(shoesList)
-//        }
-//        if(checkedSize.filter(clickedOption)){
-//          shoeFilter.push(shoeList)
-//        }
-
-// var shoeArray = [];
-// options.addEventListener("click", function(evt){
-// var valueS = evt.target.value
-// for(var i=0; i<shoeDataList.length; i++){
-// if(shoeDataList[i]!==""){
-// shoeArray.push(valueS);
-//
-// }
-// }
-
-//   var shoeStockHTML = template({ shoes: shoeDataList })
-//   stock.innerHTML = shoeArray;
-//
-// });
+  document.querySelector(".brandBox").value=""
+  document.querySelector(".colorBox").value=""
+  document.querySelector(".sizeBox").value=""
+});
